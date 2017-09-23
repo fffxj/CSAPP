954618f0648d5282eb6313ca9bba4106f41bdde2
Xiaojie Feng <fengxiaojie1997@gmail.com>
2017-09-23 13:42:51 +0800

    add my cachelab solution

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -1,7 +1,236 @@
+#include <stdio.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "cachelab.h"
 
-int main()
+/* Cache simulator states */
+#define HIT 10
+#define MISS 20
+#define MISS_EVICTION 30
+#define HIT_HIT 40
+#define MISS_HIT 50
+#define MISS_EVICTION_HIT 60
+
+/* Command line parameters */
+static int verbose;
+static int s, E, b;
+static char *tracefile;
+
+/* Simulator valuables */
+static int state;
+static int hits, misses, evictions;
+static int timestamp;
+
+/* Cache and cache line structure */
+typedef struct {
+  int valid;                    /* valid bit */
+  int tag;                      /* tag bits */
+  int timestamp;                /* cache access timestamp */
+} cache_line_t, *cache_line_ptr;
+
+typedef struct {
+  int S;                        /* number of sets */
+  int E;                        /* number of lines per set */
+  cache_line_ptr lines;
+} cache_t, *cache_ptr;
+
+/* Create SxE cache */
+cache_ptr newCache(int S, int E) {
+  /* Allocate cache header structure */
+  cache_ptr result = (cache_ptr) malloc(sizeof(cache_t));
+  cache_line_ptr lines = NULL;
+  if (!result)
+    return NULL;                /* Couldn't allocate storage */
+  result->S = S;
+  result->E = E;
+  /* Allocate cache sets */
+  if (S > 0 && E > 0) {
+    int len = S*E;
+    lines = (cache_line_ptr) calloc(len, sizeof(cache_line_t));
+    if (!lines) {
+      free((void *) result);
+      return NULL;
+    }
+    for (int i = 0; i < len; i++) {
+      lines[i].valid = 0;
+    }
+  }
+  /* Sets will either be NULL or allocated array */
+  result->lines = lines;
+  return result;
+}
+
+/* Free cache memory */
+void freeCache(cache_ptr cache) {
+  if (cache)
+    free((void *) cache->lines);
+  free((void *) cache);
+}
+
+/* Cache data load/store */
+void accessCache(cache_ptr cache, int address) {
+  int tag = (address >> (s + b)); /* address's highest bit is always 0 */
+  int set = (address >> b) & ((1 << s) - 1);
+  int E = cache->E;
+
+  cache_line_ptr searcher = cache->lines + E * set;
+  int i;  
+  /* Cache hit */
+  for (i = 0; i < E; i++) {
+    if (searcher[i].valid && searcher[i].tag == tag) {
+      searcher[i].timestamp = timestamp++;
+      state = HIT;
+      hits++;
+      return;
+    }
+  }
+  /* Cache miss */
+  for (i = 0; i < E; i++) {
+    if (!searcher[i].valid) {
+      searcher[i].valid = 1;
+      searcher[i].tag = tag;
+      searcher[i].timestamp = timestamp++;
+      state = MISS;
+      misses++;
+      return;
+    }
+  }
+  /* Cache miss, eviction */
+  cache_line_ptr evictee = searcher;
+  for (i = 0; i < E; i++) {
+    /* assert: searcher[i].valid is true */
+    if (searcher[i].timestamp < evictee->timestamp) {
+      evictee = searcher + i;
+    }
+  }
+  evictee->tag = tag;
+  evictee->timestamp = timestamp++;
+  state = MISS_EVICTION;
+  misses++;
+  evictions++;
+  return;
+}
+
+/* Cache data modify */
+void modifyCache(cache_ptr cache, int address) {
+  accessCache(cache, address);
+  hits++;
+  switch (state) {
+  case HIT:
+    state = HIT_HIT;
+    break;
+  case MISS:
+    state = MISS_HIT;
+    break;
+  case MISS_EVICTION:
+    state = MISS_EVICTION_HIT;
+    break;
+  default:
+    break;
+  }
+}
+
+/* Simulator program help message */
+void usage() {
+  printf("  Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+  printf("Options:\n");
+  printf("  -h         Print this help message.\n");
+  printf("  -v         Optional verbose flag.\n");
+  printf("  -s <num>   Number of set index bits.\n");
+  printf("  -E <num>   Number of lines per set.\n");
+  printf("  -b <num>   Number of block offset bits.\n");
+  printf("  -t <file>  Trace file.\n");
+  printf("Examples:\n");
+  printf("  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+  printf("  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
+/* Verbose mode message */
+void verboseInfo(char operation, int address, int size) {
+  printf("%c %x,%x ", operation, address, size);
+  switch (state) {
+  case HIT:
+    printf("hit");
+    break;
+  case MISS:
+    printf("miss");
+    break;
+  case MISS_EVICTION:
+    printf("miss eviction");
+    break;
+  case HIT_HIT:
+    printf("hit hit");
+    break;
+  case MISS_HIT:
+    printf("miss hit");
+    break;
+  case MISS_EVICTION_HIT:
+    printf("miss eviction hit");
+    break;
+  }
+  printf("\n");
+}
+
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
-    return 0;
+  /* Handle command line parameters */
+  int opt;
+
+  while ((opt = getopt(argc, argv, "h::v::s:E:b:t:")) != -1) {
+    switch (opt) {    
+    case 'v':
+      verbose = 1;
+      break;
+    case 's':
+      s = atoi(optarg);
+      break;
+    case 'E':
+      E = atoi(optarg);
+      break;
+    case 'b':
+      b = atoi(optarg);
+      break;
+    case 't':
+      tracefile = optarg;
+      break;
+    case 'h':
+      usage();
+      exit(0);
+    case '?':
+    default:
+      usage();
+      exit(1);
+    }
+  }  
+
+  /* Cache simulator main logic */
+  int S = 1 << s;               /* S = pow(2, s) */
+  cache_ptr cache = newCache(S, E);
+  FILE *tracefile_fp = fopen(tracefile, "r");
+  assert(tracefile_fp);
+
+  char operation;
+  int address, size;
+  while (fscanf(tracefile_fp, " %c%x,%d\n", &operation, &address, &size) > 0) {
+    switch (operation) {
+    case 'L':
+    case 'S':
+      accessCache(cache, address);
+      break;
+    case 'M':
+      modifyCache(cache, address);
+      break;
+    default:
+      break;
+    }
+    if (verbose)
+      verboseInfo(operation, address, size);
+  }
+
+  fclose(tracefile_fp);
+  freeCache(cache);
+
+  printSummary(hits, misses, evictions);
 }
--- cachelab/cachelab-handout/trans.c
+++ cachelab/cachelab-handout/trans.c
@@ -22,6 +22,145 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+  int i, j, ii, jj;
+  int t0, t1, t2, t3, t4, t5, t6, t7;
+
+  /* Transpose 32x32 matrix.
+   * 
+   * Using blocking to increase temporal locality:
+   * Partition matrix into sixteen(4x4) 8x8 submatrices, then transpose each
+   * of them.
+   * To deal with conflict misses along the matrix diagonal, use local
+   * variables (registers) to cache them.
+   */
+  if (N == 32 && M == 32) {
+    for (ii = 0; ii < N; ii += 8) {
+      for (jj = 0; jj < M; jj += 8) {
+        /* To deal with conflict misses along the diagonal,
+         * load and store one cache line each time. */
+        for (i = 0; i < 8; i++) {
+          t0 = A[ii+i][jj];
+          t1 = A[ii+i][jj+1];
+          t2 = A[ii+i][jj+2];
+          t3 = A[ii+i][jj+3];
+          t4 = A[ii+i][jj+4];
+          t5 = A[ii+i][jj+5];
+          t6 = A[ii+i][jj+6];
+          t7 = A[ii+i][jj+7];
+
+          B[jj][ii+i] = t0;
+          B[jj+1][ii+i] = t1;
+          B[jj+2][ii+i] = t2;
+          B[jj+3][ii+i] = t3;
+          B[jj+4][ii+i] = t4;
+          B[jj+5][ii+i] = t5;
+          B[jj+6][ii+i] = t6;
+          B[jj+7][ii+i] = t7;
+        }
+      }
+    }
+  }
+
+  /* Transpose 64x64 matrix:
+   *
+   * 8x8 submatrices doesn't work well, and 4x4 ones is not good enough.
+   * So we should do more work to deal with conflict misses:
+   * Divide matrix into 64(8x8) 8x8 submatrices, and in each 8x8 submatrices,
+   * divide it into 4x4 ones.
+   * Then transpose each 8x8 submatrices Ai to Bi, for i from 0 to 63.
+   *
+   * Ai     ->  Bi
+   *
+   * a0 a1  ->  b0=a0' b1=a2'
+   * a2 a3  ->  b2=a1' b3=a3'
+   *
+   * Phase 1: a0 trans to b0, a1 trans to b1.
+   * for i from 0 to 3:
+   *     Step 1: cache the ist row a0 & a1 to 8 local variables.
+   *     Step 2: move the first 4 variables to the ist column of b0.
+   *     Step 3: move the last 4 variables to the ist column of b1.
+   * Phase 2: b1 move to b2, a2 trans to b1.
+   * for i from 0 to 3:
+   *     Step 1: cache the ist row of b1 to 4 local variables.
+   *     Step 2: trans the ist column of a2 to the ist row of b1.
+   *     Step 3: move the 4 local variables to the ist b2.
+   * Phase 3: a3 trans b3.
+   */
+  else if (N == 64 && M == 64) {
+    for (ii = 0; ii < N; ii += 8) {
+      for (jj = 0; jj < M; jj += 8) {
+        // Phase 1
+        for (i = 0; i < 4; i++) {
+          t0 = A[ii+i][jj];
+          t1 = A[ii+i][jj+1];
+          t2 = A[ii+i][jj+2];
+          t3 = A[ii+i][jj+3];
+          t4 = A[ii+i][jj+4];
+          t5 = A[ii+i][jj+5];
+          t6 = A[ii+i][jj+6];
+          t7 = A[ii+i][jj+7];
+          
+          B[jj][ii+i] = t0;
+          B[jj+1][ii+i] = t1;
+          B[jj+2][ii+i] = t2;
+          B[jj+3][ii+i] = t3;
+
+          B[jj][ii+i+4] = t4;
+          B[jj+1][ii+i+4] = t5;
+          B[jj+2][ii+i+4] = t6;
+          B[jj+3][ii+i+4] = t7;
+        }
+        // Phase 2
+        for (i = 0; i < 4; i++) {
+          t0 = B[jj+i][ii+4];
+          t1 = B[jj+i][ii+5];
+          t2 = B[jj+i][ii+6];
+          t3 = B[jj+i][ii+7];
+
+          t4 = A[ii+4][jj+i];
+          t5 = A[ii+5][jj+i];
+          t6 = A[ii+6][jj+i];
+          t7 = A[ii+7][jj+i];
+
+          B[jj+i][ii+4] = t4;
+          B[jj+i][ii+5] = t5;
+          B[jj+i][ii+6] = t6;
+          B[jj+i][ii+7] = t7;
+
+          B[jj+4+i][ii] = t0;
+          B[jj+4+i][ii+1] = t1;
+          B[jj+4+i][ii+2] = t2;
+          B[jj+4+i][ii+3] = t3;          
+        }
+        // Phase 3
+        for (i = 4; i < 8; i++) {
+          t4 = A[ii+i][jj+4];
+          t5 = A[ii+i][jj+5];
+          t6 = A[ii+i][jj+6];
+          t7 = A[ii+i][jj+7];
+
+          B[jj+4][ii+i] = t4;
+          B[jj+5][ii+i] = t5;
+          B[jj+6][ii+i] = t6;
+          B[jj+7][ii+i] = t7;
+        }
+      }
+    }
+  }
+
+  /* Transpose 61x67 matrix */
+  else if (N == 67 && M == 61) {
+    for (ii = 0; ii < N; ii += 18) {
+      for (jj = 0; jj < M; jj += 18) {
+        for (i = ii; i < ii + 18 && i < N; i++) {
+          for (j = jj; j < jj + 18 && j < M; j++) {
+            B[j][i] = A[i][j];
+          }
+        }
+      }
+    }
+  }
+
 }
 
 /* 
@@ -56,10 +195,10 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    registerTransFunction(trans, trans_desc);
 
 }
 
